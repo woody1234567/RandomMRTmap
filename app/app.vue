f8ec151659fef07b56e6c8c2506bb36995a8f962
woody8873 <johannes8873@gmail.com>
2026-09-21 23:03:47 +0800

    功能：製作一個可以幫助我決定這個假日要去哪裡玩的小應用

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+useHead({ htmlAttrs: { lang: 'zh-Hant' }, meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }, { name: 'theme-color', content: '#f5f1e8' }] })
+useSeoMeta({ title: '捷運任我行｜隨機選一站', description: '按一下，讓台北捷運替你選出今天的目的地。' })
+</script>
+<template><UApp><NuxtPage /></UApp></template>
